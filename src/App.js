06578cb66a187132d90d3430283fafaa8bb604a4
junsehyeon <shjun1210@icloud.com>
2024-08-31 13:52:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import AppLayout from './layout/AppLayout'
+import Homepage from './pages/Homepage/Homepage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+//홈페이지 /
+//영화 전체 보여주는 페이지 /movie
+// 영화 디테일 페이지 /movies/:id
+function App() {
+  return (
+  <div className="App">
+    <Routes>
+      <Route path="/" element={<AppLayout/>}>//user화면
+        <Route index element={<Homepage/>}/>
+        <Route path="movies">
+          <Route index element={<MoviePage/>}/>
+          <Route path=":id" element={<MovieDetailPage/>}/>
+        </Route>
+      </Route>
+
+      <Route path="*" element={<NotFoundPage/>}/>
+    </Routes>
+  </div>  
+  );
+}
+
+export default App;
---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Outlet,Link } from 'react-router-dom';
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Navbar expand="lg" variant="dark" className="navbar-custom" >
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <img width={80} src='https://cdnweb01.wikitree.co.kr/webdata/editor/202305/09/img_20230509113254_b805310c.webp' alt="Brand Logo"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/movies">Movies</Nav.Link>
+            
+            
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-danger" >Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Outlet/>
+    </div>
+  )
+}
+
+export default AppLayout
