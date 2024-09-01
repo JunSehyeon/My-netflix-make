83083aa8fbc37f3701cab8547ed590a7e0c576b2
junsehyeon <shjun1210@icloud.com>
2024-09-01 17:35:55 +0100

    second commit

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,23 @@
+import {useQuery} from "@tanstack/react-query";
+import api from "../utils/api"
+const fetchPopularMovies =async () => {
+ 
+        return await api.get('/movie/popular');
+}   
+const testFetchPopularMovies = async () => {
+    try {
+      const response = await fetchPopularMovies();
+      console.log("Fetched popular movies:", response.data);
+    } catch (error) {
+      console.error("Error fetching popular movies:", error);
+    }
+  };
+  
+  testFetchPopularMovies();
+export const usePopularMoviesQuery=()=>{
+    return useQuery({
+        queryKey: ['movie-popular'],
+        queryFn: fetchPopularMovies,
+        select:(result)=>result.data,
+    })
+}
---
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { usePopularMoviesQuery } from '../../../../hook/usePopularMovies'
+import ClipLoader from "react-spinners/ClipLoader"
+
+import Alert from 'react-bootstrap/Alert';
+import './Banner.style.css'
+const Banner = () => {
+    const {data,isLoading, error,isError}=usePopularMoviesQuery();
+    console.log("ddd",data)
+    if (isLoading) {return <div className="bigContainer"><ClipLoader color ="#f88c6b" loading={isLoading} size={70}/></div> } ;
+    if (isError) return<Alert  variant="danger">{error.message}</Alert>;
+  
+  return (
+    <div >
+  
+    <div style={{backgroundImage:"url("+`https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data.results[0].poster_path}`+")"}}
+    className="banner">
+     
+   <div className="text-white banner-text-area">
+<h1>{data?.results[0].title}</h1>
+<p>{data && data.results[0].overview}</p>
+   </div>
+    </div>
+    </div>
+  )
+}
+
+export default Banner
